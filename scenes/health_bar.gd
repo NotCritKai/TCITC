9154bcd3d204d28daa758be7727fa65c4974a339
Lanceber <lanceber@icloud.com>
2025-12-18 08:13:27 -0500

    coin

--- scenes/health_bar.gd
+++
@@ -1,6 +0,0 @@
-extends ProgressBar
-
-# Called when Player emits the healthChanged signal
-func update(new_health: int) -> void:
-	value = new_health   # sets the bar’s value to match Player’s health
-	print("Health bar updated to:", new_health)
