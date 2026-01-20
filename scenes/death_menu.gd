7ff9dc54f501c710436e6b1e36f4e305913c37d4
Lanceber <lanceber@icloud.com>
2026-01-20 12:27:33 -0500

    Shop_Buttons

--- scenes/death_menu.gd
+++ scenes/death_menu.gd
@@ -1,8 +1,15 @@
 extends Control
 
-func _on_try_again_pressed():
+
+func _on_return_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/Scene1GAME.tscn")
+	
+#Built in Godot Code that cancels a scene- 
+#-when ESC is pressed and goes to whatever is told 
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().change_scene_to_file("res://scenes/Scene1GAME.tscn")
 
-func _on_quit_pressed():
-	get_tree().quit()
-
+	
+	
+	
