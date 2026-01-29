819b08e43e472ae1420402f1cec1439552eeb1fd
Lanceber <lanceber@icloud.com>
2026-01-29 13:51:23 -0500

    shop button

--- scripts/gamemanager.gd
+++ scripts/gamemanager.gd
@@ -10,8 +10,8 @@ func add_point():
 
 #BUTTON CODES
 func _on_go_to_shop_button_pressed():
-	var shop = preload("res://scenes/shop_menu.tscn").instantiate()
-	get_tree().paused = true
+		get_tree().change_scene_to_file("res://scenes/shop_menu.tscn")
+	
 #BUTTON CODES
 
 
