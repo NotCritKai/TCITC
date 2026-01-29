823dbe7c942a2738d71d13a07e2336717abc891e
NotCritKai <braylonringo525@gmail.com>
2026-01-29 13:55:02 -0500

    Shop Style

--- scripts/Coin.gd
+++ scripts/Coin.gd
@@ -2,8 +2,10 @@ extends Area2D
 
 @onready var game_manager = %GameManager
 @onready var pickup_coins_animation = $PickupCoinsAnimation
+@onready var coins_l = $Coins_L
 
 
 func _on_body_entered(_body):
 	game_manager.add_point()
 	pickup_coins_animation.play("pickup")
+
--- scripts/gamemanager.gd
+++ scripts/gamemanager.gd
@@ -1,13 +1,18 @@
 extends Node
 
-var score = 0
+var score = 1
 
 @onready var coin_label = $CanvasLayer/CoinLabel
+@onready var coins_l = $Coins_L
 
 func add_point():
 	score += 1
 	coin_label.text =str(score) + " 💰"
-
+	coins_l.text =str(score) + " 💰"
+	
+func _ready():
+	if score > 0:
+		coins_l.text =score + str(" 💰")
 #BUTTON CODES
 func _on_go_to_shop_button_pressed():
 		get_tree().change_scene_to_file("res://scenes/shop_menu.tscn")
