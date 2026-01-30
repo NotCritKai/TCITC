db594b0c542ffec9bdc37ccd17c4ada19b1db4df
Lanceber <lanceber@icloud.com>
2026-01-30 13:19:22 -0500

    OtherChangers

--- scripts/death_menu.gd
+++ scripts/death_menu.gd
@@ -5,8 +5,7 @@ func _ready():
 
 #BUTTON CODES
 func _on_return_button_pressed():
-	get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/Scene1GAME.tscn")
+	get_tree().paused = false and get_tree().change_scene_to_file("res://scenes/Scene1GAME.tscn")
 	
 #Built in Godot Code that cancels a scene- 
 #-when ESC is pressed and goes to whatever is told 
