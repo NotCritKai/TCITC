807bc669bc818749f0f2a2de7414057d714a876d
Lanceber <lanceber@icloud.com>
2026-02-05 13:41:11 -0500

    SHOP_FINAL

--- scripts/gamemanager.gd
+++ scripts/gamemanager.gd
@@ -5,14 +5,18 @@ var score = 1
 @onready var coin_label = $CanvasLayer/CoinLabel
 @onready var coins_l = $Coins_L
 
-func add_point():
+func add_point():	
 	score += 1
 	coin_label.text =str(score) + " 💰"
 	coins_l.text =str(score) + " 💰"
 
 #BUTTON CODES
 func _on_go_to_shop_button_pressed():
-	get_tree().paused = true and get_tree().change_scene_to_file("res://scenes/shop_menu.tscn")
+	var shop_scene = load("res://scenes/shop_menu.tscn")  # ← must match EXACT path
+	var shop = shop_scene.instantiate()
+	get_tree().current_scene.get_node("GameManager/CanvasLayer2").add_child(shop)
+	get_tree().paused = true
+	 
 	
 #BUTTON CODES
 
