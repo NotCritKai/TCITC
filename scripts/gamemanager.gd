db56502a454730727110b8196026a71d20cc75b6
NotCritKai <braylonringo525@gmail.com>
2026-02-05 13:52:59 -0500

    Coin In Shop Menu (WIP)

--- scripts/gamemanager.gd
+++ scripts/gamemanager.gd
@@ -2,13 +2,14 @@ extends Node
 
 var score = 1
 
-@onready var coin_label = $CanvasLayer/CoinLabel
+@onready var coin_label = $CanvasLayer2/CoinLabel
 @onready var coins_l = $Coins_L
+@onready var shop_menu = $"."
 
 func add_point():	
 	score += 1
 	coin_label.text =str(score) + " 💰"
-	coins_l.text =str(score) + " 💰"
+	shop_menu.coin_label.text =str(score) + " 💰"
 
 #BUTTON CODES
 func _on_go_to_shop_button_pressed():
