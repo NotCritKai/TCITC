cedc902791a080ac70923407913b28ca8ab1ab91
Lanceber <lanceber@icloud.com>
2026-02-10 12:41:09 -0500

    idk

--- scripts/gamemanager.gd
+++ scripts/gamemanager.gd
@@ -8,16 +8,13 @@ var score = 1
 func add_point():	
 	score += 1
 	coin_label.text =str(score) + " 💰"
-	coins_l.text =str(score) + " 💰"
 
 #BUTTON CODES
 func _on_go_to_shop_button_pressed():
-	var shop_scene = load("res://scenes/shop_menu.tscn")  # ← must match EXACT path
-	var shop = shop_scene.instantiate()
+	var shop_scene = load("res://scenes/shop_menu.tscn")#loads thw shop  
+	var shop = shop_scene.instantiate()#initiates shop 
 	get_tree().current_scene.get_node("GameManager/CanvasLayer2").add_child(shop)
-	get_tree().paused = true
-	 
-	
-#BUTTON CODES
+	get_tree().paused = true #pauses
+#BUTTON CODE
 
 
