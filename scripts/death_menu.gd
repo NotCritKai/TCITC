5ef0a5335b93bd21421cad3a5aa0d26783460662
Lanceber <lanceber@icloud.com>
2026-02-17 12:53:48 -0500

    LB_Changes

--- scripts/death_menu.gd
+++ scripts/death_menu.gd
@@ -7,3 +7,10 @@ func _on_return_button_pressed():
 	queue_free()
 	
 #BUTTON CODES
+
+func _on_try_again_pressed():
+		get_tree().change_scene_to_file("res://scenes/Scene1GAME.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
