56bae7515f6341ee91f89e925f5dea6019677493
Lanceber <lanceber@icloud.com>
2026-02-18 10:43:55 -0500

    Character_Selection

--- scripts/Main_Menu.gd
+++ scripts/Main_Menu.gd
@@ -12,7 +12,7 @@ func _process(_delta):
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/Scene1GAME.tscn")
+	get_tree().change_scene_to_file("res://scenes/Character_Selction.tscn")
 
 
 func _on_quit_pressed():
