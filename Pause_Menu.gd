1240dbd57f907ce04104783ef89d94cdd10ad7db
Lanceber <lanceber@icloud.com>
2026-02-20 10:34:17 -0500

    Pause_Scene_Fix

--- Pause_Menu.gd
+++ Pause_Menu.gd
@@ -7,13 +7,12 @@ func _on_quit_button_pressed():
 	get_tree().quit()
 
 func _on_credits_button_pressed():
-	var credits_scene = load("res://scenes/credits.tscn")#loads the menu  
-	var credits = credits_scene.instantiate()#initiates scene 
 	$Credits.visible = true
 	
 
 func _on_unpause_btn_pressed():
-	get_tree().paused = false
 	$Credits.visible = false
-	queue_free()  
+	get_tree().paused = false
+	queue_free()
+
 
